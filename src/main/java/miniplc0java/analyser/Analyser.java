b6e1a0e93083da6a574cbfe0f615da64cd4d288e
zyang946 <54517903+zyang946@users.noreply.github.com>
2020-10-26 13:58:00 +0800

    add

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -446,6 +446,7 @@ public final class Analyser {
         } else if (check(TokenType.LParen)) {
             // 是表达式
             // 调用相应的处理函数
+            expect(TokenType.LParen);
             analyseExpression();
             expect(TokenType.RParen);
         } else {
